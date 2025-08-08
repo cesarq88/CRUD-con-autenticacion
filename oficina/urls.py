7b0eb2712ff4db3a8dd9114580c151c926ca41ba
cesarq88 <cesarq.089@gmail.com>
2025-08-08 20:50:03 -0300

    se hizo busqueda por nombre y nombre corto , se actualiza el listado para que tenga barra de busqueda y botones modifica y elimnar , se aseguran las rutas urls.py y la vista oficina_list para soportar ambos filytos

--- oficina/urls.py
+++ oficina/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('oficinas/', views.oficina_list, name='oficina_list'),
     path('oficinas/nueva/', views.oficina_create, name='oficina_create'),
-
+    path('oficinas/<int:pk>/editar/', views.oficina_update, name='oficina_update'),
+    path('oficinas/<int:pk>/eliminar/', views.oficina_delete, name='oficina_delete'),
+    
 ]
 
--- oficina/views.py
+++ oficina/views.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from .models import Oficina
@@ -12,7 +12,7 @@ class OficinaForm(forms.ModelForm):
 @login_required
 def oficina_list(request):
     oficinas = Oficina.objects.all().order_by('nombre')
-    page_obj = Paginator(oficinas, 10).get_page(request.GET.get('page'))
+    page_obj = Paginator(oficinas, 5).get_page(request.GET.get('page'))
     return render(request, 'oficina/oficina_list.html', {'page_obj': page_obj})
 
 @login_required
@@ -25,4 +25,24 @@ def oficina_create(request):
     else:
         form = OficinaForm()
     return render(request, 'oficina/oficina_form.html', {'form': form})
+
+@login_required
+def oficina_update(request, pk):
+    oficina = get_object_or_404(Oficina, pk=pk)
+    if request.method == 'POST':
+        form = OficinaForm(request.POST, instance=oficina)
+        if form.is_valid():
+            form.save()
+            return redirect('oficina_list')
+    else:
+        form = OficinaForm(instance=oficina)
+    return render(request, 'oficina/oficina_form.html', {'form': form})
+
+@login_required
+def oficina_delete(request, pk):
+    oficina = get_object_or_404(Oficina, pk=pk)
+    if request.method == 'POST':
+        oficina.delete()
+        return redirect('oficina_list')
+    return render(request, 'oficina/oficina_confirm_delete.html', {'oficina': oficina})
 
