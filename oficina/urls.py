e3aae300f1371bf2eb2e1374fe9a324394cd639d
cesarq88 <cesarq.089@gmail.com>
2025-08-08 18:51:51 -0300

    Se implemeta el alta de oficina, formulario, plantilla oficina_form.html, boton en oficina_list.html con acceso restringido a usuarios logueados

--- oficina/urls.py
+++ oficina/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('oficinas/', views.oficina_list, name='oficina_list'),
+    path('oficinas/nueva/', views.oficina_create, name='oficina_create'),
+
 ]
 
--- oficina/views.py
+++ oficina/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
-from .models import Oficina
+from django import forms
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from .models import Oficina
 
+class OficinaForm(forms.ModelForm):
+    class Meta:
+        model = Oficina
+        fields = ['nombre', 'nombre_corto']
 
 @login_required
 def oficina_list(request):
     oficinas = Oficina.objects.all().order_by('nombre')
-
-    paginator = Paginator(oficinas, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = Paginator(oficinas, 10).get_page(request.GET.get('page'))
     return render(request, 'oficina/oficina_list.html', {'page_obj': page_obj})
 
-    
-# Create your views here.
+@login_required
+def oficina_create(request):
+    if request.method == 'POST':
+        form = OficinaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('oficina_list')
+    else:
+        form = OficinaForm()
+    return render(request, 'oficina/oficina_form.html', {'form': form})
+
